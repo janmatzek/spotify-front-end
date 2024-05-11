e07e809b38c1b7c24d13147be6bd65d76322b84b
janmatzek <75334114+janmatzek@users.noreply.github.com>
2024-05-11 19:24:39 +0200

    added page for artists and genres

---
+++ src/FavoriteArtists.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, Link } from "@chakra-ui/react";
+import LoadingIndicator from "./LoadingIndicator"; // Import LoadingIndicator component
+
+const FavoriteArtists = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchTableData();
+  }, []);
+
+  const fetchTableData = async () => {
+    try {
+      const response = await fetch(process.env.REACT_APP_FAVORITE_ARTISTS_URL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json(); // Parse the JSON response
+      console.log(data); // Log the parsed JSON data
+      setData(data); // Set the state with the parsed JSON data
+      setLoading(false); // Update loading state
+    } catch (error) {
+      console.error("Fetch error:", error);
+      setError(error); // Set error state if there's an error
+      setLoading(false); // Update loading state
+    }
+  };
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="artists-table-container">
+      {/* <Heading {...headingStyles}>TOP 5 TRACKS</Heading> */}
+      {loading ? (
+        <LoadingIndicator />
+      ) : (
+        <Table variant="simple" size="sm">
+          <Thead>
+            <Tr>
+              <Th>TOP 5 ARTISTS</Th>
+              <Th>NAME</Th>
+              <Th>BB INDEX</Th>
+              <Th>GENRE</Th>
+              <Th>TRACKS PLAYED</Th>
+              <Th>LINK</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((item, index) => (
+              <Tr key={index}>
+                <Td>
+                  <img
+                    src={item.images_url}
+                    alt={item.name}
+                    style={{ maxWidth: "100px" }}
+                  />
+                </Td>
+                <Td>{item.name}</Td>
+                <Td>{item.popularity}</Td>
+                <Td>{item.main_genre}</Td>
+                <Td>{item.count_tracks}</Td>
+                <Td>
+                  <Link href={item.external_urls_spotify} isExternal>
+                    🎵
+                  </Link>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
+    </div>
+  );
+};
+
+export default FavoriteArtists;
